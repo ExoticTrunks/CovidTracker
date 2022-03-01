2e1bee0b34851db11e2e8e943b528d7a24ce28fc
ExoticTrunks <ralipalermo@hotmail.com>
2022-03-01 17:09:14 +0800

    base website complete

---
+++ src/components/Header.vue
@@ -0,0 +1,15 @@
+<template>
+  <header class="text-center bg-blue-600 text-white p-4 mb-10">
+    <div class="text-3xl md:text-5xl font-bold ">Covid-19 Tracker ☠ </div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+  props: {
+
+  }
+}
+</script>
+
